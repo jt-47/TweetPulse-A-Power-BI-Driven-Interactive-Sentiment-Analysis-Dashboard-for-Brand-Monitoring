e93a0d162d3e5f26e5ed2a3ecec623fed50b66ba
Jerry Thomas <167605309+jt-47@users.noreply.github.com>
2025-04-08 00:36:52 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+MODELS_DIR = r'H:\My Drive\Twitter 2024\Models'
+
+# Scaler and model file paths
+BERT_SCALER_PATH = f"{MODELS_DIR}/bert_scaler.pkl"
+SCALER_PATH = f"{MODELS_DIR}/scaler.pkl"
+XGB_MODEL_PATH = f"{MODELS_DIR}/xgb_bert.pkl"
+
+# Path for CSV storage
+DATA_DIR = r'G:\My Drive\Twitter 2024'
+RAW_TWEETS_FILE = f"{DATA_DIR}/tweets4.csv"
+PROCESSED_TWEETS_FILE = f"{DATA_DIR}/tweets4 - tweets4.csv"
